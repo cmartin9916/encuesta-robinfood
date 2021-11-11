747fde92e09a148f5032f733b0b3fe1dfc249970
cmartin9916 <cmartin.9916@gmail.com>
2021-11-10 23:18:52 -0500

    Add: Estructura proyecto API Rest - Encuesta RobinFood

---
+++ EncuestaRobinFood/robinfood-api/src/main/java/com/co/cmartin/rf/EncuestaRobinFoodApplication.java
@@ -0,0 +1,30 @@
+/**
+ * @autor Carlos Martin
+ * @version 0.0.1 10/11/2021
+ *
+ */
+package com.co.cmartin.rf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Carlos Martin
+ * @version 0.0.1 10/11/2021
+ *
+ */
+@SpringBootApplication
+public class EncuestaRobinFoodApplication {
+
+	/**
+	 * @author Carlos Martin
+	 * @version 0.0.1 10/11/2021
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.setProperty("server.servlet.context-path", "/parameta-rest");
+		
+		SpringApplication.run(EncuestaRobinFoodApplication.class, args);
+	}
+
+}
---
+++ EncuestaRobinFood/robinfood-api/src/test/java/com/co/cmartin/rf/EncuestaRobinFoodApplicationTests.java
@@ -0,0 +1,22 @@
+/**
+ * @autor Carlos Martin
+ * @version 0.0.1 10/11/2021
+ *
+ */
+package com.co.cmartin.rf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Carlos Martin
+ * @version 0.0.1 10/11/2021
+ *
+ */
+@SpringBootTest
+public class EncuestaRobinFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
